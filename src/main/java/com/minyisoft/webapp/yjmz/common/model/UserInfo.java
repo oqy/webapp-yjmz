86a760aa9d5d63ab3a04332930886abdd34f7240
oqy <qingyong.ou@gmail.com>
2014-08-10 21:45:37 +0800

    init setting

---
+++ src/main/java/com/minyisoft/webapp/yjmz/common/model/UserInfo.java
@@ -0,0 +1,32 @@
+package com.minyisoft.webapp.yjmz.common.model;
+
+import com.minyisoft.webapp.core.model.CoreBaseInfo;
+import com.minyisoft.webapp.core.model.ISystemUserObject;
+
+public class UserInfo extends CoreBaseInfo implements ISystemUserObject {
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getUserLoginName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getUserPassword() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getUserPasswordSalt() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
