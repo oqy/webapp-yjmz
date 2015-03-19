3d47a227f12fc29ff0a9289013836a937f910d8f
oqy <qingyong.ou@gmail.com>
2015-03-19 20:34:17 +0800

    工作流任务更改执行人时增加微信模板消息提示

--- src/main/java/com/minyisoft/webapp/yjmz/common/web/admin/WorkFlowProcessController.java
+++ src/main/java/com/minyisoft/webapp/yjmz/common/web/admin/WorkFlowProcessController.java
@@ -6,9 +6,13 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.activiti.engine.HistoryService;
 import org.activiti.engine.RuntimeService;
+import org.activiti.engine.TaskService;
 import org.activiti.engine.history.HistoricProcessInstance;
+import org.activiti.engine.task.Task;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.time.DateFormatUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,7 +27,9 @@ import com.minyisoft.webapp.yjmz.common.model.UserInfo;
 import com.minyisoft.webapp.yjmz.common.model.WorkFlowBusinessModel;
 import com.minyisoft.webapp.yjmz.common.model.WorkFlowConfigInfo;
 import com.minyisoft.webapp.yjmz.common.model.criteria.UserOrgRelationCriteria;
+import com.minyisoft.webapp.yjmz.common.model.entity.WeixinTemplateMessage;
 import com.minyisoft.webapp.yjmz.common.security.SecurityUtils;
+import com.minyisoft.webapp.yjmz.common.service.MessageService;
 import com.minyisoft.webapp.yjmz.common.service.UserOrgRelationService;
 import com.minyisoft.webapp.yjmz.common.service.WorkFlowProcessService;
 import com.minyisoft.webapp.yjmz.common.service.WorkFlowTaskService;
@@ -47,6 +53,12 @@ public class WorkFlowProcessController extends BaseController {
 	private UserOrgRelationService userOrgRelationService;
 	@Autowired
 	private WorkFlowTaskService workFlowTaskService;
+	@Autowired
+	private TaskService taskService;
+	@Autowired
+	private MessageService messageService;
+	@Value("#{applicationProperties['web.domain']}")
+	private String webDomain;
 
 	/**
 	 * 获取运行中的工作流程实例列表
@@ -146,14 +158,34 @@ public class WorkFlowProcessController extends BaseController {
 	 * 更改任务执行人
 	 */
 	@RequestMapping(value = "changeTaskAssignee.html", method = RequestMethod.POST, params = "workFlowModelId")
-	public String changeTaskAssignee(@RequestParam(value = "workFlowModelId", required = false) String workFlowModelId,
+	public String changeTaskAssignee(
+			@RequestParam(value = "workFlowModelId", required = false) WorkFlowBusinessModel businessModel,
 			@RequestParam(value = "taskId", required = false) String taskId,
 			@RequestParam(value = "assignee", required = false) UserInfo assignee) {
-		if (StringUtils.isNotBlank(taskId) && assignee != null) {
+		if (businessModel != null && assignee != null) {
 			workFlowTaskService.changeAssignee(taskId, assignee);
+
+			Task task = null;
+			if (StringUtils.isNotBlank(assignee.getWeixinOpenId())
+					&& (task = taskService.createTaskQuery().taskId(taskId).singleResult()) != null) {
+				// 微信模板消息待发送内容
+				String[] weixinTemplateMessages = new String[6];
+				weixinTemplateMessages[0] = "您好，您有一条新的待处理工作流任务\n";
+				weixinTemplateMessages[1] = businessModel.getProcessInstanceName();
+				weixinTemplateMessages[2] = task.getName();
+				weixinTemplateMessages[3] = businessModel.getCreateUser().getName();
+				weixinTemplateMessages[4] = DateFormatUtils.format(task.getCreateTime(), "yyyy年M月d日HH时mm分");
+				weixinTemplateMessages[5] = "\n请您及时处理";
+
+				messageService.sendWeixinTemplateMessage(assignee.getWeixinOpenId(),
+						WeixinTemplateMessage.PENDING_TRANSACTION_NOTIFY, webDomain + "/viewDetail.html?billId="
+								+ businessModel.getId(), weixinTemplateMessages);
+			}
+
 		}
-		if (StringUtils.isNotBlank(workFlowModelId)) {
-			return "redirect:workFlowDetail.html?workFlowModelId=" + workFlowModelId;
+
+		if (businessModel != null) {
+			return "redirect:workFlowDetail.html?workFlowModelId=" + businessModel.getId();
 		}
 		return "redirect:processInstances.html";
 	}
